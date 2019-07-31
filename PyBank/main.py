ae33a23f56344ae4821ce7651dc5931efb236251
Lorsmo <51829277+Lorsmo@users.noreply.github.com>
2019-07-31 12:03:55 -0500

    code reduced and first lines of output code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,55 +1,43 @@
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
+# Import the os module to create file paths across operating systems
 import os
 
 # Module for reading CSV files
 import csv
 
+# Set path for file
 budget_csvpath = os.path.join('Resources', 'budget_data.csv')
 
-# Read in the CSV file
+# Open and read in the CSV file
 with open(budget_csvpath, newline='') as csvfile:
 
-    # Split the data on commas
+    # CSV reader specifies delimiter (split the data on commas), and variable that holds contents
     csv_reader = csv.reader(csvfile, delimiter=',')
-
+    
+    # Read the header row first
     header = next(csv_reader)
-  
-    counter_row = 0
-    first_data, last_data = [], []
+    
     profit_losses = []
     month = []
-    Imont = 0
+
     # Loop through the data
     for row in csv_reader:
-        
-        counter_row +=1
-
-        if counter_row == 1:
-            first_data = int(row[1])
-
-        if counter_row == counter_row:
-            last_data = int(row[1])
-
-        
-        #profit_losses.append(int(row[1]))
-        #p = int(row[1])
-        #if p == max(str(profit_losses)):
-            #Imonth = row[0]
 
         profit_losses.append(int(row[1]))
+        
         month.append(str(row[0]))
-        
-        
-    average_change = round(((last_data - first_data) / (counter_row - 1)), 2)
  
-
     print("Financial Analysis")
     print("----------------------------")
-
     print(f"Total Months: {len(month)}")
     print(f"Total: ${sum(profit_losses)}")
-    print(f"Average Changes: ${average_change}")
+    print(f"Average Changes: ${round(((profit_losses[len(month)-1] - profit_losses[0]) / (len(month) - 1)), 2)}")
     print(f"Greatest Increase in Profits: {month[profit_losses.index(max(profit_losses))]}  (${max(profit_losses)})")
     print(f"Greatest Decrease in Profits: {month[profit_losses.index(min(profit_losses))]}  (${min(profit_losses)})")
-    
+
+# Specify the file to write to
+# output_path = os.path.join("Analysis", "Financial_Analysis")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+# with open(output_path, "w") as text_file:
+ #   print(def print_analysis)
+  #  txtwriter = csv.writer(text_file)
