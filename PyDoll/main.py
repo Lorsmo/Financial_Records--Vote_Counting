37d6afe2d27a2b0e958728aa2b15831d0c549a43
Lorsmo <51829277+Lorsmo@users.noreply.github.com>
2019-08-01 10:17:06 -0500

    count added

--- PyDoll/main.py
+++ PyDoll/main.py
@@ -5,3 +5,32 @@ import os
 # Module for reading CSV files
 import csv
 
+election_csvpath = os.path.join('Resources', 'light_election_data.csv')
+
+with open(election_csvpath, newline='') as csvfile:
+
+    # Split the data on commas
+    csv_reader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csv_reader)
+
+    candidat_list = []
+    candidates = []
+    for row in csv_reader:
+
+        candidates.append(row[2])
+
+        for candidate in candidates:
+            if candidate not in candidat_list:
+                candidat_list.append(candidate)
+            
+                
+
+    
+    print("Election Results")
+    print("----------------------")
+    print(f"Total Votes: {len(candidates)}")
+    print("----------------------")
+    for i in range (len(candidat_list)):
+        candidates.count(candidat_list[i])
+        print(f"{candidat_list[i]}: {candidates.count(candidat_list[i])}")
