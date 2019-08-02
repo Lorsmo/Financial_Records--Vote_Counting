f69b7acc59808a569eac89d50ae057a7bbe9ae77
Lorsmo <51829277+Lorsmo@users.noreply.github.com>
2019-08-02 13:38:54 -0500

    presentation modified

--- PyDoll/main.py
+++ PyDoll/main.py
@@ -18,7 +18,7 @@ with open(election_csvpath, newline='') as csvfile:
     header = next(csv_reader)
 
     # Lists to store data
-    candidat_list = []
+    candidate_list = []
     votes = []
     
     # Loop through the data
@@ -26,29 +26,47 @@ with open(election_csvpath, newline='') as csvfile:
         
         # Add each votes to the list "votes"
         votes.append(str(row[2]))
+
+        # Variable total_votes equal to lenght of column vote
+        total_votes = len(votes)
         
         # If the vote is for a candidate not present in the candidate list
-        if str(row[2]) not in candidat_list:
+        if str(row[2]) not in candidate_list:
 
             # Put the candidate's name in the candidate list
-            candidat_list.append(str(row[2]))
+            candidate_list.append(str(row[2]))
 
 # Print the election results       
 print("-----------------------")  
 print("Election Results")
 print("-----------------------")
-print(f"Total Votes: {len(votes)}")
+print(f"Total Votes: {total_votes}")
 print("-----------------------")
-for i in range (len(candidat_list)):
-    print(f"{candidat_list[i]}: {round((votes.count(candidat_list[i]))/len(votes)*100, 3)}% ({votes.count(candidat_list[i])})")
-    if votes.count(candidat_list[i]) > votes.count(candidat_list[i-1]):
-        winner = candidat_list[i]
+
+# Loop through the candidates into the list
+for i in range (len(candidate_list)):
+    
+    # For readability, assign values to variables with descriptive names
+    candidate = candidate_list[i]        
+    votes_count = votes.count(candidate_list[i])
+    previous_votes_count = votes.count(candidate_list[i-1])
+    percentage_votes = round((votes_count/total_votes)*100, 3)
+    
+    # print the results for each candidate
+    print(f"{candidate}: {percentage_votes}% ({votes_count})")
+    
+    # If number of votes for the currently candidate is greater than for the previous one, 
+    if votes_count > previous_votes_count:
+
+        # the variable winner equal the name of this candidate
+        winner = candidate
+
 print("-----------------------")
 print(f"Winner: {winner}")
 print("-----------------------")
 
 #Specify the file to write to
-output_path = os.path.join("Election_Results", f"Election Results {candidat_list}.txt")
+output_path = os.path.join("Election_Results", f"Election Results {candidate_list}.txt")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
 with open(output_path, "w", newline="") as f:
@@ -59,10 +77,10 @@ with open(output_path, "w", newline="") as f:
     print(("-----------------------"), file =f)
     print((f"Total Votes: {len(votes)}"), file =f)
     print(("-----------------------"), file =f)
-    for i in range (len(candidat_list)):
-        print((f"{candidat_list[i]}: {round((votes.count(candidat_list[i]))/len(votes)*100, 3)}% ({votes.count(candidat_list[i])})"), file =f)
-        if votes.count(candidat_list[i]) > votes.count(candidat_list[i-1]):
-            winner = candidat_list[i]
+    for i in range (len(candidate_list)):
+        print((f"{candidate_list[i]}: {round((votes.count(candidate_list[i]))/len(votes)*100, 3)}% ({votes.count(candidate_list[i])})"), file =f)
+        if votes.count(candidate_list[i]) > votes.count(candidate_list[i-1]):
+            winner = candidate_list[i]
     print(("-----------------------"), file =f)
     print((f"Winner: {winner}"), file =f)
     print(("-----------------------"), file =f)
