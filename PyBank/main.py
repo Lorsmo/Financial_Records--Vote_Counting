6ef2565ed5b6542fc6a5a4da380b808c0e722de2
Lorsmo <51829277+Lorsmo@users.noreply.github.com>
2019-08-03 09:06:51 -0500

    modif calcul increase decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,16 +19,23 @@ with open(budget_csvpath, newline='') as csvfile:
     # Lists to store data
     profit_losses = []
     month = []
+    increase_decrease = []
 
     # Loop through the data
     for row in csv_reader:
 
+        sum_inc_dec = 0
+        
         # Add the profit and losses
         profit_losses.append(int(row[1]))
         
         # Add the months
         month.append(str(row[0]))
-    
+        
+    # Calculate the difference of profit or losses between two months and store it into increase_decrease list
+    for i in range(len(profit_losses)):
+        increase_decrease.append(int(profit_losses[i]) - int(profit_losses[i-1]))
+        
     # print the results in the terminal
     print("---------------------------------------------")
     print(F"Financial Analysis from {month[0]} to {month[len(month)-1]}")
@@ -36,11 +43,9 @@ with open(budget_csvpath, newline='') as csvfile:
     print(f"Total Months: {len(month)}")
     print(f"Total: ${sum(profit_losses)}")
     print(f"Average Changes: ${round(((profit_losses[len(month)-1] - profit_losses[0]) / (len(month) - 1)), 2)}")
-    print(f"Greatest Increase in Profits: {month[profit_losses.index(max(profit_losses))]}  (${max(profit_losses)})")
-    print(f"Greatest Decrease in Profits: {month[profit_losses.index(min(profit_losses))]}  (${min(profit_losses)})")
+    print(f"Greatest Increase in Profits: {month[increase_decrease.index(max(increase_decrease))]}  (${max(increase_decrease)})")
+    print(f"Greatest Decrease in Profits: {month[increase_decrease.index(min(increase_decrease))]}  (${min(increase_decrease)})")
     print("---------------------------------------------")
-  
-    
 
 #Specify the file to write to
 output_path = os.path.join("Analysis", f"Financial_Analysis {month[0]} - {month[len(month)-1]}.txt")
@@ -55,8 +60,8 @@ with open(output_path, "w", newline="") as f:
     print((f"Total Months: {len(month)}"), file =f)
     print((f"Total: ${sum(profit_losses)}"), file =f)
     print((f"Average Changes: ${round(((profit_losses[len(month)-1] - profit_losses[0]) / (len(month) - 1)), 2)}"), file =f)
-    print((f"Greatest Increase in Profits: {month[profit_losses.index(max(profit_losses))]}  (${max(profit_losses)})"), file =f)
-    print((f"Greatest Decrease in Profits: {month[profit_losses.index(min(profit_losses))]}  (${min(profit_losses)})"), file =f)
+    print((f"Greatest Increase in Profits: {month[increase_decrease.index(max(increase_decrease))]}  (${max(increase_decrease)})"), file =f)
+    print((f"Greatest Decrease in Profits: {month[increase_decrease.index(min(increase_decrease))]}  (${min(increase_decrease)})"), file =f)
     print(("---------------------------------------------"), file =f)
 
     
